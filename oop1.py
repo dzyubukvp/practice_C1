deb28bdd03e901a0a4a38ce18ef3749dd140da06
dzyubukvp <dzyubukvp@yandex.ru>
2022-12-03 15:07:54 +0400

    создать класс кот с именем, возрастом, полом

--- oop1.py
+++ oop1.py
@@ -12,3 +12,16 @@ class Rectangle:
     # Метод, рассчитывающий площадь
     def get_area(self):
         return self.width * self.height
+class Cat:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self. age = age
+    def get_name(self):
+        return self.name
+
+    def get_gender(self):
+        return self.gender
+
+    def get_age(self):
+        return self.age
--- testRectangle.py
+++ testRectangle.py
@@ -1,16 +1,19 @@
-from oop1 import Rectangle
+from oop1 import Cat
 
-r1 = Rectangle(10, 5)
-r2 = Rectangle(7, 12)
+r1 = Cat('Вася', 'Мальчик', '3')
+r2 = Cat('Дуся', 'сучка', '2')
 
-print("r1.width =", r1.width)
-print("r1.height =", r1.height)
-print("r1.get_width =", r1.get_width())
-print("r1.get_height =", r1.get_height())
-print("r1.get_area =", r1.get_area())
+#print("r1.width =", r1.width)
+#print("r1.height =", r1.height)
+#print("r1.get_width =", r1.get_width())
+#print("r1.get_height =", r1.get_height())
+#print("r1.get_area =", r1.get_area())
 
-print("r2.width =", r2.width)
-print("r2.height =", r2.height)
-print("r2.get_width =", r2.get_width())
-print("r2.get_height =", r2.get_height())
-print("r2.get_area =", r2.get_area())
+#print("r2.width =", r2.width)
+#print("r2.height =", r2.height)
+#print("r2.get_width =", r2.get_width())
+#print("r2.get_height =", r2.get_height())
+#print("r2.get_area =", r2.get_area())
+
+print(f'Кот {r1.get_age()} годичного возраста {r1.get_gender()} зовут {r1.get_name()}')
+print(f'Кот {r2.get_age()} годичного возраста {r2.get_gender()} зовут {r2.get_name()}')
